6786f2cc2d019ac32d230560a032550da94de93e
Dimitar Goshevski <roundrobine@gmail.com>
2020-09-08 19:51:36 +0200

    fix bugs in movie inventory dto tests

--- src/test/java/com/roundrobine/movie/rentals/service/dto/MovieInventoryDTOTest.java
+++ src/test/java/com/roundrobine/movie/rentals/service/dto/MovieInventoryDTOTest.java
@@ -11,12 +11,12 @@ public class MovieInventoryDTOTest {
         MovieInventoryDTO movieInventoryDTO1 = new MovieInventoryDTO();
         movieInventoryDTO1.setId(1L);
         MovieInventoryDTO movieInventoryDTO2 = new MovieInventoryDTO();
-        assertThat(movieInventoryDTO1).isNotEqualTo(movieInventoryDTO2);
+        assertThat(movieInventoryDTO1.getId()).isNotEqualTo(movieInventoryDTO2.getId());
         movieInventoryDTO2.setId(movieInventoryDTO1.getId());
-        assertThat(movieInventoryDTO1).isEqualTo(movieInventoryDTO2);
+        assertThat(movieInventoryDTO1.getId()).isEqualTo(movieInventoryDTO2.getId());
         movieInventoryDTO2.setId(2L);
-        assertThat(movieInventoryDTO1).isNotEqualTo(movieInventoryDTO2);
+        assertThat(movieInventoryDTO1.getId()).isNotEqualTo(movieInventoryDTO2.getId());
         movieInventoryDTO1.setId(null);
-        assertThat(movieInventoryDTO1).isNotEqualTo(movieInventoryDTO2);
+        assertThat(movieInventoryDTO1.getId()).isNotEqualTo(movieInventoryDTO2.getId());
     }
 }
